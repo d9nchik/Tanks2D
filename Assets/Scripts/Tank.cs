e40215331dc4ce9a042fa653008303d101877b39
d9nich <54186666+d9nchik@users.noreply.github.com>
2022-10-23 18:35:10 +0300

    add tank movement

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    private readonly float speedForce = 15f;
+    public float maxSpeed = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");// -1 to 1
+
+        rb.AddForce(moveX * speedForce * Vector2.right);
+
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
